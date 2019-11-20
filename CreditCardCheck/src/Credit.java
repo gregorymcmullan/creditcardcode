befafcb66318fa6b76de2b00b56c1815fdf8fd82
gregory.mcmullan <gregory.mcmullan@MU-C1-08.mullen.pvt.k12.co.us>
2019-11-20 12:24:15 -0700

    another committ

---
+++ CreditCardCheck/src/Credit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Credit
+{
+	static long ccv = (long) 5424180123456789.;
+	static long ccinv=(long)5424180123456788.;
+	static long digits;
+	static long remainder;	
+	static long[]validNumbers=new long[16];
+	public static void main(String[] args)
+		
+			{
+	
+		for(int i=validNumbers.length-1;i>=0;i++) {
+			digits = remainder%10;
+			remainder=remainder/10;
+			
+			
+		}
+			
+
+			}
+
+	}
