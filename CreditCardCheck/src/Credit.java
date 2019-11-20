232d6cceb6ec10c95906a8d65739a1e7a49d09ac
gregory.mcmullan <gregory.mcmullan@MU-C1-08.mullen.pvt.k12.co.us>
2019-11-20 12:24:51 -0700

    actual committ

--- CreditCardCheck/src/Credit.java
+++ CreditCardCheck/src/Credit.java
@@ -1,23 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 public class Credit
-{
-	static long ccv = (long) 5424180123456789.;
-	static long ccinv=(long)5424180123456788.;
-	static long digits;
-	static long remainder;	
-	static long[]validNumbers=new long[16];
-	public static void main(String[] args)
-		
+	{
+		static long ccv = (long) 5424180123456789.;
+		static long ccinv = (long) 5424180123456788.;
+		static long digits;
+		static long remainder;
+		static long[] validNumbers = new long[16];
+
+		public static void main(String[] args)
 			{
-	
-		for(int i=validNumbers.length-1;i>=0;i++) {
-			digits = remainder%10;
-			remainder=remainder/10;
-			
-			
+int totalLines = 0;
+int lineNumber=0;
+
+
+try {
+	Scanner file = new Scanner(new File("cardnumber.txt"));
+	while(file.hasNextLong()) {
+		
+		lineNumber++;
+		long ccn = file.nextLong();
+		if(luhnCheck(ccn)) {
+			System.out.println(ccn);
+			totalLines++;
 		}
-			
+	}
+
+}catch(FileNotFoundException e) {
+	e.printStackTrace();
+}
+
+System.out.println("the list has " + totalLines);
+
+
+
+
+
 
 			}
 
+public static boolean luhnCheck(long ccn) {
+	return false;
+	
+}
 	}
